23c565a96e4ae43efe02ca4e4a31e79b85ba12b0
SwapnaKandimalla9 <swapnaejjagani@gmail@gmail.com>
2024-10-22 13:50:59 +0530

    daily report of functions

---
+++ 21-10-2024/functions.py
@@ -0,0 +1,40 @@
+#Functions in python
+function is a Block of code ,runs only when it is called
+and it is also a block of reusable block of code executes whwnever we call the function
+we are passing the data to this function as an arguments
+
+Two types of inbuilt functions
+input():allows us to read input from the user
+print():it will print whatever we print on console
+
+User Defined Functions
+these functions perform a particular task to processs 
+
+Advantages of fuctions 
+reusable
+modularity
+maintainence
+
+Declaration of function 
+using of def keyword
+function name after the def keyword and parantheses containing any parameters follwed by :
+
+example:
+def myfunction(name):
+    print("hi hello")
+#example 2
+average of two numbers
+def average(a,b):
+    print("avaerage of two numbers is",(a+b)/2)
+    
+    average(10,20)
+    output:avaerage of two numbers is 15.0
+
+#return
+    def avaerage(a,b)
+        return (a+b)/2
+    
+    result = average(10,20)
+    print(result)
+
+
