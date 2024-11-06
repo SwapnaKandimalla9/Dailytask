cf64321494278ea1cb4d975ea2c9b8993e141f9c
SwapnaKandimalla9 <swapnaejjagani@gmail@gmail.com>
2024-11-06 08:36:26 +0530

    nested class

---
+++ 5-11-2024/nestedclass.py
@@ -0,0 +1,29 @@
+#nested class:- it is nothing but a class in a class
+
+#example:-
+class employee:
+    def __init__(self,name,age,skillset):
+        self.name = name
+        self.age = age
+        self.skillset = skillset
+    
+    def display(self):
+        print(self.name)
+        print(self.age)
+        print(self.skillset)
+    
+    class employement:
+        def __init__(self,cmpnyname,salary,domain):
+            self.cmpnyname = cmpnyname
+            self.salary = salary
+            self.domain = domain
+        def display(self):
+            print(self.cmpnyname)
+            print(self.salary)
+            print(self.domain)
+    
+    m = employement("skywaves",30000,"python")
+    m.display()
+
+p = employee("swapna",25,"backend developer")
+p.display()
